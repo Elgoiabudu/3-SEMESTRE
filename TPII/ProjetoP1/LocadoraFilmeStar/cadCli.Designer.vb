ba4ae15ebe87ce489a9b5e6da0f5d492b6bcb442
Fernando Bonde Leonelli <fernandobondeleonelli@gmail.com>
2023-09-02 17:28:00 -0300

    Atualização do cadCli(FORM)

--- TPII/ProjetoP1/LocadoraFilmeStar/cadCli.Designer.vb
+++ TPII/ProjetoP1/LocadoraFilmeStar/cadCli.Designer.vb
@@ -23,16 +23,106 @@ Partial Class cadCli
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(cadCli))
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        TextBox3 = New TextBox()
+        Label4 = New Label()
+        TextBox4 = New TextBox()
         SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(12, 41)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(127, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 23)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(40, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Nome"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(145, 23)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(68, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "Sobrenome"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(145, 41)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(115, 23)
+        TextBox2.TabIndex = 2
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 67)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(22, 15)
+        Label3.TabIndex = 5
+        Label3.Text = "RG"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(12, 85)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(127, 23)
+        TextBox3.TabIndex = 4
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(145, 67)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(28, 15)
+        Label4.TabIndex = 7
+        Label4.Text = "CPF"
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(145, 85)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(115, 23)
+        TextBox4.TabIndex = 6
         ' 
         ' cadCli
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Label4)
+        Controls.Add(TextBox4)
+        Controls.Add(Label3)
+        Controls.Add(TextBox3)
+        Controls.Add(Label2)
+        Controls.Add(TextBox2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
         Icon = CType(resources.GetObject("$this.Icon"), Icon)
         Name = "cadCli"
         Text = "Cadastro de Clientes"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox4 As TextBox
 End Class
