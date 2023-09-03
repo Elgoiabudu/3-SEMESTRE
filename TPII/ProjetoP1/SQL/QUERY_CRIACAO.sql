e1c9a4a98e788b8615bb170483fb55a78b8b520b
Fernando Bonde Leonelli <fernandobondeleonelli@gmail.com>
2023-09-03 08:43:58 -0300

    Update QUERY_CRIACAO.sql

--- TPII/ProjetoP1/SQL/QUERY_CRIACAO.sql
+++ TPII/ProjetoP1/SQL/QUERY_CRIACAO.sql
@@ -17,34 +17,9 @@ CREATE TABLE tbClientes (
 				DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE tbFornec (
-	ID INT PRIMARY KEY IDENTITY(1,1),
-	nome VARCHAR(150) NOT NULL,
-	email VARCHAR(150) NOT NULL,
-	CNPJ VARCHAR (150) UNIQUE NOT NULL,
-	telefone VARCHAR(14) NOT NULL,
-	endereco VARCHAR(255) NOT NULL,
-    cidade VARCHAR(100) NOT NULL,
-    estado VARCHAR(50) NOT NULL,
-	situacao VARCHAR (20) NOT NULL
-);
-
-INSERT INTO tbFornec (nome, email, CNPJ, telefone, endereco, cidade, estado, situacao)
-VALUES
-('Fornecedor A', 'fornecedorA@email.com', '12345678901234', '123-456-7890', 'Rua A, 123', 'Cidade A', 'Estado A', 'Ativo'),
-('Fornecedor B', 'fornecedorB@email.com', '23456789012345', '234-567-8901', 'Rua B, 456', 'Cidade B', 'Estado B', 'Ativo'),
-('Fornecedor C', 'fornecedorC@email.com', '34567890123456', '345-678-9012', 'Rua C, 789', 'Cidade C', 'Estado C', 'Ativo'),
-('Fornecedor D', 'fornecedorD@email.com', '45678901234567', '456-789-0123', 'Rua D, 101', 'Cidade D', 'Estado D', 'Ativo'),
-('Fornecedor E', 'fornecedorE@email.com', '56789012345678', '567-890-1234', 'Rua E, 222', 'Cidade E', 'Estado E', 'Ativo'),
-('Fornecedor F', 'fornecedorF@email.com', '67890123456789', '678-901-2345', 'Rua F, 333', 'Cidade F', 'Estado F', 'Ativo'),
-('Fornecedor G', 'fornecedorG@email.com', '78901234567890', '789-012-3456', 'Rua G, 444', 'Cidade G', 'Estado G', 'Ativo'),
-('Fornecedor H', 'fornecedorH@email.com', '89012345678901', '890-123-4567', 'Rua H, 555', 'Cidade H', 'Estado H', 'Ativo'),
-('Fornecedor I', 'fornecedorI@email.com', '90123456789012', '901-234-5678', 'Rua I, 666', 'Cidade I', 'Estado I', 'Ativo'),
-('Fornecedor J', 'fornecedorJ@email.com', '01234567890123', '012-345-6789', 'Rua J, 777', 'Cidade J', 'Estado J', 'Ativo');
-
-SELECT * FROM tbFornec
-
-select * from tbClientes
+ALTER TABLE tbClientes 
+ADD
+foto VARCHAR(250)
 
 INSERT INTO tbClientes (nome, sobrenome, email, cpf, rg, telefoneCel, telefoneFixo, endereco, cidade, estado, CEP)
 VALUES ('João', 'Silva', 'joao.silva@email.com', '123.456.789-01', '1234567', '555-5555', '555-5555', 'Rua A', 'São Paulo', 'SP', '12345-678');
@@ -93,6 +68,30 @@ VALUES ('Felipe', 'Ramos', 'felipe.ramos@email.com', '333.444.555-66', '3334445'
 
 select * from tbClientes
 
-ALTER TABLE tbClientes 
-ADD
-foto VARCHAR(250)
+CREATE TABLE tbFornec (
+	ID INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(150) NOT NULL,
+	email VARCHAR(150) NOT NULL,
+	CNPJ VARCHAR (150) UNIQUE NOT NULL,
+	telefone VARCHAR(14) NOT NULL,
+	endereco VARCHAR(255) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    estado VARCHAR(50) NOT NULL,
+	situacao VARCHAR (20) NOT NULL
+);
+
+INSERT INTO tbFornec (nome, email, CNPJ, telefone, endereco, cidade, estado, situacao)
+VALUES
+('Fornecedor A', 'fornecedorA@email.com', '12345678901234', '123-456-7890', 'Rua A, 123', 'Cidade A', 'Estado A', 'Ativo'),
+('Fornecedor B', 'fornecedorB@email.com', '23456789012345', '234-567-8901', 'Rua B, 456', 'Cidade B', 'Estado B', 'Ativo'),
+('Fornecedor C', 'fornecedorC@email.com', '34567890123456', '345-678-9012', 'Rua C, 789', 'Cidade C', 'Estado C', 'Ativo'),
+('Fornecedor D', 'fornecedorD@email.com', '45678901234567', '456-789-0123', 'Rua D, 101', 'Cidade D', 'Estado D', 'Ativo'),
+('Fornecedor E', 'fornecedorE@email.com', '56789012345678', '567-890-1234', 'Rua E, 222', 'Cidade E', 'Estado E', 'Ativo'),
+('Fornecedor F', 'fornecedorF@email.com', '67890123456789', '678-901-2345', 'Rua F, 333', 'Cidade F', 'Estado F', 'Ativo'),
+('Fornecedor G', 'fornecedorG@email.com', '78901234567890', '789-012-3456', 'Rua G, 444', 'Cidade G', 'Estado G', 'Ativo'),
+('Fornecedor H', 'fornecedorH@email.com', '89012345678901', '890-123-4567', 'Rua H, 555', 'Cidade H', 'Estado H', 'Ativo'),
+('Fornecedor I', 'fornecedorI@email.com', '90123456789012', '901-234-5678', 'Rua I, 666', 'Cidade I', 'Estado I', 'Ativo'),
+('Fornecedor J', 'fornecedorJ@email.com', '01234567890123', '012-345-6789', 'Rua J, 777', 'Cidade J', 'Estado J', 'Ativo');
+
+SELECT * FROM tbFornec
+
