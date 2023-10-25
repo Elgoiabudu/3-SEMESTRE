faa5b1d0adcab7f0504f5b17b2461d526d29541c
Elgoiabudu <fernandobondeleonelli@gmail.com>
2023-10-24 21:47:06 -0300

    Refatoração da Solução

--- TPII/Segunda Parte do Semestre/CRUDNOVO/Form1.Designer.cs
+++ TPII/Segunda Parte do Semestre/CRUDNOVO/Form1.Designer.cs
@@ -1,16 +1,16 @@
-﻿namespace CRUDNOVO
+﻿namespace CrudNovo
 {
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Variável de designer necessária.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Limpar os recursos que estão sendo usados.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +20,115 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Código gerado pelo Windows Form Designer
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
         /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_CPF = new System.Windows.Forms.MaskedTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btn_salvar = new System.Windows.Forms.ToolStripButton();
+            this.btn_atualizar = new System.Windows.Forms.ToolStripButton();
+            this.btn_apagar = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_CPF
+            // 
+            this.txt_CPF.Location = new System.Drawing.Point(12, 45);
+            this.txt_CPF.Mask = "999,999,999-99";
+            this.txt_CPF.Name = "txt_CPF";
+            this.txt_CPF.Size = new System.Drawing.Size(100, 20);
+            this.txt_CPF.TabIndex = 0;
+            this.txt_CPF.Leave += new System.EventHandler(this.txt_CPF_Leave);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txt_nome.Location = new System.Drawing.Point(12, 84);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(290, 20);
+            this.txt_nome.TabIndex = 2;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btn_salvar,
+            this.btn_atualizar,
+            this.btn_apagar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(326, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btn_salvar
+            // 
+            this.btn_salvar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btn_salvar.Image = ((System.Drawing.Image)(resources.GetObject("btn_salvar.Image")));
+            this.btn_salvar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btn_salvar.Name = "btn_salvar";
+            this.btn_salvar.Size = new System.Drawing.Size(23, 22);
+            this.btn_salvar.Text = "toolStripButton1";
+            this.btn_salvar.Click += new System.EventHandler(this.btn_salvar_Click);
+            // 
+            // btn_atualizar
+            // 
+            this.btn_atualizar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btn_atualizar.Image = ((System.Drawing.Image)(resources.GetObject("btn_atualizar.Image")));
+            this.btn_atualizar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btn_atualizar.Name = "btn_atualizar";
+            this.btn_atualizar.Size = new System.Drawing.Size(23, 22);
+            this.btn_atualizar.Text = "toolStripButton2";
+            // 
+            // btn_apagar
+            // 
+            this.btn_apagar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btn_apagar.Image = ((System.Drawing.Image)(resources.GetObject("btn_apagar.Image")));
+            this.btn_apagar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btn_apagar.Name = "btn_apagar";
+            this.btn_apagar.Size = new System.Drawing.Size(23, 22);
+            this.btn_apagar.Text = "toolStripButton3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(326, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.txt_nome);
+            this.Controls.Add(this.txt_CPF);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MaskedTextBox txt_CPF;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btn_salvar;
+        private System.Windows.Forms.ToolStripButton btn_atualizar;
+        private System.Windows.Forms.ToolStripButton btn_apagar;
     }
 }
+
--- TPII/Segunda Parte do Semestre/CRUDNOVO/Form1.cs
+++ TPII/Segunda Parte do Semestre/CRUDNOVO/Form1.cs
@@ -1,10 +1,107 @@
-namespace CRUDNOVO
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrudNovo
 {
     public partial class Form1 : Form
     {
+        SqlConnection conexao;
+        SqlCommand comando;
+        string SQL;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conexao = new SqlConnection(@"Server=LAB5-13;
+                                        Database=cadastroMagno;
+                                        trusted_connection=yes");
+
+            try
+            {
+                conexao.Open();
+                MessageBox.Show("Conexão Bem Magna!", "AVISO",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Conexão Não Magna!", "AVISO",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_salvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string cpf = txt_CPF.Text;
+
+                string consultaSQL = "SELECT * FROM tb_cadastro WHERE cpf ='" + cpf + "'"; // Substitua isso pela sua consulta SQL.
+
+                using (SqlCommand command = new SqlCommand(consultaSQL, conexao))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            MessageBox.Show("CPF Já Cadastrado!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else
+                        {
+                            SQL = "INSERT INTO tb_cadastro(cpf, nome) values (@CPF, @NOME)";
+                            comando = new SqlCommand(SQL, conexao);
+                            comando.Parameters.AddWithValue("@CPF", txt_CPF.Text);
+                            comando.Parameters.AddWithValue("@NOME", txt_nome.Text);
+                            comando.ExecuteNonQuery();
+                            MessageBox.Show("Dados Gravados Com Sucesso!", "SUCESSO",
+                                             MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            txt_CPF.Clear();
+                            txt_nome.Clear();
+                            txt_CPF.Focus();
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Erro ao Gravar o Registro!", "ERRO",
+                                 MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txt_CPF_Leave(object sender, EventArgs e)
+        {
+            string cpf = txt_CPF.Text;
+
+            string consultaSQL = "SELECT * FROM tb_cadastro WHERE cpf ='"+cpf+"'"; // Substitua isso pela sua consulta SQL.
+
+            using (SqlCommand command = new SqlCommand(consultaSQL, conexao))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        string resultado = reader["nome"].ToString(); // Substitua "Nome" pelo nome da coluna desejada.
+                        txt_nome.Text = resultado; // Exibindo o resultado em uma caixa de texto.
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nenhum resultado encontrado.");
+                    }
+                }
+            }
+
         }
     }
 }
